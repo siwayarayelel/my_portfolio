110982e393f940384e4379c4a72d2ba4291f145d
eysii <cyrilllirio143@gmail.com>
2023-02-05 08:53:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+let sideMenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sideMenu.style.right = "0";
+}
+function closemenu(){
+    sideMenu.style.right = "-200px";
+}
