d6dbda32be054759e2098e5b62528edc1a313134
eysii <cyrilllirio143@gmail.com>
2023-02-05 09:26:04 +0800

    second commit

--- app.js
+++ app.js
@@ -20,3 +20,20 @@ function openmenu(){
 function closemenu(){
     sideMenu.style.right = "-200px";
 }
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzkrOf9YbMhWYPwRBkPWrlO60drmn-XT1_QRbD9hFMmGH6lBMgaRJNQM_wQmsXcuyUqVQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully";
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
